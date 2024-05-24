aba99b7d867d0618000adfc5a0a57ea71ce029c5
jonatas venancio <jvenancio167@hotmail.com>
2024-05-24 08:57:10 -0300

    Modify account factory to use account_number sequence and balance

--- spec/factories/account.rb
+++ spec/factories/account.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :account do
-    sequence(:numero_conta) { |n| n.to_s.rjust(6, '0') }
-    saldo { 200.00 }
+    sequence(:account_number) { |n| n.to_s.rjust(6, '0') }
+    balance { 200.00 }
   end
 end
