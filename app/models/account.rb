3f48855e29ee293fcea91bccbd235d47ddab49af
jonatas venancio <jvenancio167@hotmail.com>
2024-05-24 08:53:12 -0300

    Implement alias attributes numero_conta and saldo in Account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,9 @@
 class Account < ApplicationRecord
   has_many :amount_transactions, dependent: :destroy
 
+  alias_attribute :numero_conta, :account_number
+  alias_attribute :saldo, :balance
+
   validates :numero_conta, presence: true, uniqueness: true
   validates :saldo, numericality: { greater_than_or_equal_to: 0 }
 end
